6241cade5d1fbb87b1e25527303007e8578c1e8d
ashyT-Cloud <ashytech.innovate@gmail.com>
2026-04-10 07:36:56 +0000

    Working app for CI

--- Day-5-github-actions/Day-1/commands.md
+++
@@ -1,6 +0,0 @@
-# Commands Used
-
-sudo apt update
-sudo apt install nginx -y
-systemctl status nginx
-curl localhost     --------#EC2 IP--------
---
+++ Day-5-github-actions/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.write("Hello from Day 5 CI/CD");
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000")
+});
+
